11bba7420fe9352e33b440da8000d1bb9a4aba7f
Ishan Nitin Sohony <ishangalbatorix@gmail.com>
2017-03-27 22:09:34 +0530

    Update and rename mnist_keras2.py to mnist_keras_kaggle_final.py

--- mnist_keras_kaggle_final.py
+++ mnist_keras_kaggle_final.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 27 21:07:34 2017
-
-@author: nodlehs
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
 Created on Mon Mar 27 19:29:40 2017
 
 @author: nodlehs
@@ -46,9 +38,6 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
-X_train = numpy.concatenate((X_train,X_test),axis = 0)
-y_train = numpy.concatenate((y_train,y_test),axis = 0)
-
 print(numpy.shape(X_train))
 print(numpy.shape(y_train))
 
@@ -73,7 +62,7 @@ def larger_model():
 # build the model
 model = larger_model()
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=10, batch_size=200, verbose=2)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=20, batch_size=200, verbose=2)
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Baseline Error: %.2f%%" % (100-scores[1]*100))
@@ -90,6 +79,6 @@ print (test_pred)
 
 if not os.path.exists('/home/nodlehs/results/'):
     os.makedirs('/home/nodlehs/results/')
-numpy.savetxt('/home/nodlehs/results/mnist-predictions%s.csv' , numpy.c_[range(1, len(test_pred) + 1), test_pred], delimiter = ',', header = 'ImageId,Label', comments = '', fmt='%d')
+numpy.savetxt('/home/nodlehs/results/mnist-predictions.csv' , numpy.c_[range(1, len(test_pred) + 1), test_pred], delimiter = ',', header = 'ImageId,Label', comments = '', fmt='%d')
 print("Saved predictions to a CSV file.")
 
